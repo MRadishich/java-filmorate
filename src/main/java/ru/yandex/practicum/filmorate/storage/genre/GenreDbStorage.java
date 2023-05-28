0557947f3f155b159ce15dc3cb3eac0bd66e9194
MRadishich <mradishich@gmail.com>
2023-05-28 22:50:46 +0300

    refactor: убрать избыточные импорты

--- src/main/java/ru/yandex/practicum/filmorate/storage/genre/GenreDbStorage.java
+++ src/main/java/ru/yandex/practicum/filmorate/storage/genre/GenreDbStorage.java
@@ -4,7 +4,6 @@ import ru.yandex.practicum.filmorate.model.film.Film;
 import ru.yandex.practicum.filmorate.model.film.Genre;
 
 import java.util.List;
-import java.util.Objects;
 import java.util.Optional;
 
 public interface GenreDbStorage {
